b63f26d63cd94d8d269e2149b9b37506f95d06bc
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2022-10-18 17:31:32 -0400

    create barebones website structure based on 2022-10-18 team meeting

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
 
 def create_app():
     app = Flask(__name__)
@@ -7,3 +10,35 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
 
     return app
+
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = '03745fbcd1b452edcd98a9acb4a0f90d'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
+
+    from .models import User, Note
+    create_database(app)
+    
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    # login_manager.login_view = 'auth.login'
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
