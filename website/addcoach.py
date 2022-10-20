411d2cad32c1ed6debdda9efc8c84675f14d3f8e
kdputn24 <kdputn24@colby.edu>
2022-10-20 11:15:15 -0400

    adding the bare minimum of the adduser pages for each type of user

---
+++ website/addcoach.py
@@ -0,0 +1,41 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User, Note
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+#from flask_login import login_user, login_required, logout_user, current_user
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/sign-up', methods= ['GET', 'POST'])
+def sign_up():
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        firstname = request.form.get('firstname')
+        lastname = request.form.get('lastname')
+        # need to add team dropdown here
+        
+
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash('Username already exists.', category='error')
+#        elif len(username) < 4:
+#            flash('Username must be greater than 3 characters.', category='error')
+#        elif len(first_name) < 2:
+#            flash('First name must be greater than 1 character.', category='error')
+#        elif len(password1) < 7:
+#            flash('Password must be at least 7 characters.', category='error')
+#        elif password1 != password2:
+#            flash('Passwords don\'t match.', category='error')
+        else:
+            # add user to database
+            new_user = User(username=username, firstname=firstname, password=generate_password_hash(password, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user, remember=True)
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template("signup.html", user=current_user)
