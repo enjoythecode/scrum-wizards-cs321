25e026634b02850b8c2a0dd450154c4f5fe5f8ff
Chandrachud-2003 <chandrachudg@gmail.com>
2022-10-20 23:59:15 -0400

    Added User Model close #51

--- website/models.py
+++ website/models.py
@@ -35,15 +35,26 @@ from sqlalchemy.sql import func
 # - content
 # on 2022-10-18, Sinan jumped 75 (/100)
 
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     first_name = db.Column(db.String(150))
+    last_name = db.Column(db.String(150))
     password = db.Column(db.String(150))
-    notes = db.relationship('Note')
+    teams = db.relationship('Team')
+    permissions_assigned = db.relationship('Permission')
+
+
+
+# class Note(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     data = db.Column(db.String(10000))
+#     date = db.Column(db.DateTime(timezone=True), default=func.now())
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+# class User(db.Model, UserMixin):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(150), unique=True)
+#     first_name = db.Column(db.String(150))
+#     password = db.Column(db.String(150))
+#     notes = db.relationship('Note')
