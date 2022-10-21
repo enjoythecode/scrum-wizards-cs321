102c057a4e68a507608bfc99fa3aa94a9bfe0000
Chandrachud-2003 <chandrachudg@gmail.com>
2022-10-21 13:12:08 -0400

    Trying to add and query data

--- website/__init__.py
+++ website/__init__.py
@@ -22,6 +22,9 @@ def create_app():
         if not path.exists('website/' + DB_NAME):
             db.create_all()
             print('Created Database!')
+            addDummyData()
+            queryDummyData()
+
 
 
     login_manager = LoginManager()
@@ -34,7 +37,35 @@ def create_app():
 
     return app
 
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
+# def create_database(app):
+#     if not path.exists('website/' + DB_NAME):
+#         db.create_all(app=app)
+#         print('Created Database!')
+
+def addDummyData():
+    # Create a dummy user
+    from .models import User, Permission, Team, Entry
+    user = User()
+    user.email = 'cmgowd25@colby.edu'
+    user.first_name = 'Chandra'
+    user.last_name = 'Gowda'
+    user.set_password('password')
+    user.permissions = Permission.ADMIN
+    db.session.add(user)
+    db.session.commit()
+    print("DB Committed")
+
+# Print all users
+def printAllUsers():
+    from .models import User, Permission, Team, Entry
+    users = User.query.all()
+    print(users)
+    print("DB printed all users")
+
+# Query dummy data
+def queryDummyData():
+    from .models import User, Permission, Team, Entry
+    user = User.query.filter_by('first_name' == 'Chandra').first()
+    print(user)
+    print("DB printed query")
+    
