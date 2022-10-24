6654eea4ecba7a61850beda4caccd6ffe461d92b
kdputn24 <kdputn24@colby.edu>
2022-10-24 14:20:48 -0400

    changed Permissions model to have relationship with User, changed permissions attributes, defined permissions in athlete permissions

--- website/athletepermissions.py
+++ website/athletepermissions.py
@@ -1,6 +1,6 @@
 
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from .models import User
+from .models import User, Permission
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
@@ -9,7 +9,7 @@ from website import views
 athletepermissions = Blueprint('athletepermissions', __name__)
 
 @athletepermissions.route('/superadmin/athletepermissions.html', methods= ['POST'])
-def athlete_permissions_form_submission(id):
+def athlete_permissions_form_submission(id = current_user.id):
 
     if request.method == 'POST':
 
@@ -19,6 +19,7 @@ def athlete_permissions_form_submission(id):
         print(deleteaccount, team, switchrole)
 
 
+        athlete_permissions = Permission(users = current_user, restricted_to_season = False, can_view_self_entries = True, can_edit_self_entries = False, can_view_own_teams_entries = False, can_edit_own_teams_entries = False, can_view_all_entries = False, can_edit_all_entries = False)
 
         user = User.query.get(id)
         user.team = team
