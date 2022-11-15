7543696fe187b7e681da04f00bc8188446d72b48
Sinan Yumurtaci <sinan.yumurtaci@gmail.com>
2022-11-15 16:17:36 -0500

    add testing

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from website import create_app
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+
+    yield app
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
