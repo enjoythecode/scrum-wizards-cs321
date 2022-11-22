ab6764111b5cff66e20dda12052b760671614494
Ghailan Fadah <ghailanfadah@Ghailans-MacBook-Pro.local>
2022-11-21 22:27:13 -0500

    added test methods for the login for the coach and athlete

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,3 +1,4 @@
+'''
 import pytest
 from website import create_test_app
 from website import models
@@ -52,3 +53,35 @@ def test_goto_coach_permissions(client):
     print(response2.data)
     assert response2.status_code == 200
     assert b'Team' in response2.data
+
+
+def test_athlete_login(client):
+
+    response = client.get('/')
+    assert response.status_code == 302 # redirect to login page
+    assert b'Redirecting' in response.data
+    assert b'login' in response.data
+
+    client.post("/login", 
+            data={"email": "jasper@gmail.com",
+                        "password": "1234"})
+
+    response = client.get('/', follow_redirects=True)
+    assert response.status_code == 200 # redirect to login page
+    assert b'Notes:' in response.data
+
+def test_coach_login(client):
+
+    response = client.get('/')
+    assert response.status_code == 302 # redirect to login page
+    assert b'Redirecting' in response.data
+    assert b'login' in response.data
+
+    client.post("/login", 
+            data={"email": "kelly@gmail.com",
+                        "password": "1234"})
+
+    response = client.get('/', follow_redirects=True)
+    assert response.status_code == 200 # redirect to login page
+    assert b'Team:' in response.data
+'''
