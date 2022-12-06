42871a16583eeb8aee292f51171699537b1ce4b8
kdputn24 <kdputn24@colby.edu>
2022-12-05 21:46:55 -0500

    tried to make change/save for permissions and deleting accounts work with html forms, not yet functional

--- website/coachpermissions.py
+++ website/coachpermissions.py
@@ -9,48 +9,44 @@ from website import views
 coachpermissions = Blueprint('coachpermissions', __name__)
 
 @coachpermissions.route('/superadmin/coachpermissions.html', methods= ['POST'])
-def coach_permissions_form_submission():
+def coach_permissions_form_submission(user):
 
-    if request.method == 'POST':
+    # changing permissions
+    if request.method == 'POST' and request.id == 'changeandsave':
 
-        deleteaccount = request.form.get('deleteaccount')
         team = request.form.get('team')
-        role = request.form.get('role')
-        save = request.form.get('save')
-        print(deleteaccount, team, role)
+        role = request.form.get('switchrole')
+        print(team, role)
 
-        # permissions
-        view_teams_entries = request.form.get('viewteamsentries')
-        edit_teams_entries = request.form.get('editteamsentries')
-        view_team_outofseason = request.form.get('outofseason')
+        # these permissiosn could be wrong
+        user.Permission = Permission(users = current_user, restricted_to_season = True, can_view_self_entries = True, can_edit_self_entries = False, can_view_own_teams_entries = True, can_edit_own_teams_entries = False, can_view_all_entries = False, can_edit_all_entries = False)
 
+    # deleting user
+    elif request.method == 'POST' and request.id == 'delete':
+        
+        user = User.query.get(user.id) # how to know which id to get
 
-
-        current_user.permissions = Permission(users = current_user, restricted_to_season = True, can_view_self_entries = True, can_edit_self_entries = True, can_view_own_teams_entries = True, can_edit_own_teams_entries = False, can_view_all_entries = False, can_edit_all_entries = False)
-
-        # user = User.query.get(id)
-        # user.team = team
-        # user.role = switchrole
-
-        # if deleteaccount:
-        #     remove(id)
-
-        return redirect("/superadmin/home.html")
-
-
-
-
-
-# deleting a user
-@coachpermissions.route("/remove/<string:id>", methods=['GET', "POST"])
-@login_required
-def remove(id):
-    user = User.query.get(id)
-
-    if user:
-        if user.user_id == current_user.id:
+        if user:
             db.session.delete(user)
             db.session.commit()
             flash('User deleted!', category='success')
 
     return redirect("/superadmin/home.html")
+
+
+
+
+
+# # deleting a user
+# @coachpermissions.route("/remove/<string:id>", methods=['GET', "POST"])
+# @login_required
+# def remove(id):
+#     user = User.query.get(id)
+
+#     if user:
+#         if user.user_id == current_user.id:
+#             db.session.delete(user)
+#             db.session.commit()
+#             flash('User deleted!', category='success')
+
+#     return redirect("/superadmin/home.html")
