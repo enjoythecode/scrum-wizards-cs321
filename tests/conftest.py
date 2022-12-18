7a10f6ce23c54f6eaa193a226c906a066b08ebb8
Chandrachud-2003 <chandrachudg@gmail.com>
2022-12-18 00:32:20 -0500

    Refactored conftest.py closes #223

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,23 @@
+# Importing the pytest module for testing
 import pytest
+# Importing the create_test_app function from the website package
 from website import create_test_app
 
+# Creating a fixture for the app
 @pytest.fixture()
 def app():
-
+    # Creating a test app
     app = create_test_app()
+    # Configuring the app
     with app.app_context():
+        # Setting the testing flag to True
         app.config.update({
         "TESTING": True,
         })
-
         yield app
 
-
+# Creating a fixture for the client
 @pytest.fixture()
 def client(app):
+    # Returning the test client
     return app.test_client()
